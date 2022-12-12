90e7d62ca542dc99dccdafba5a8d2059f9c0cf6d
tnhao <trannhathao1516@gmail.com>
2022-12-12 22:16:49 +0700

    update create slide

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,19 @@
 export const URL_EXPIRE_TIME = 7 * 24 * 60 * 60 * 100; // a week
+export const SLIDE_TYPE = {
+  MUL_CHOICES: 1,
+};
+
+export const FIRST_SLIDE = {
+  name: "Your first slide",
+  slides: [
+    {
+      detail: {
+        title: "Your question",
+        type: SLIDE_TYPE.MUL_CHOICES,
+        options: ["option 1", "option 2"],
+        values: [0, 0],
+      },
+    },
+  ],
+  groups: [],
+};
